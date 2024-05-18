a43dfa368b4340125449d5eb3b89d528e35a0250
suntea3 <166766723+suntea3@users.noreply.github.com>
2024-05-18 02:20:47 -0300

    Add files via upload

---
+++ Nombre.js
@@ -0,0 +1,18 @@
+
+//Primer ejercicio//
+var nombre = "Santiago"
+var apellido = "Olivera"
+
+console.log (nombre + " " + apellido)
+
+//Segundo ejercicio//
+
+var numero_1 = 4
+var numero_2 = 2
+
+var suma = (numero_1 + numero_2)
+console.log ("El valor de la suma es: " + suma)
+//Tercer ejercicio//
+
+var conjunto = (suma + 5)
+console.log ("El valor de la variable suma y el número elegido es: " + conjunto )
